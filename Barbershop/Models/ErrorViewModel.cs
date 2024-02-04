8005d033b3e840b7c4a887fd40bb722a7048956d
Adam Brennan <atbrenna@lakeheadu.ca>
2024-02-04 15:42:53 -0500

    Add project files.

---
+++ Barbershop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Barbershop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
