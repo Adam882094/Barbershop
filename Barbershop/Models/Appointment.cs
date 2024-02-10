0955f6341635faeb79e4ad1bba0c85acf2a8c17b
Adam Brennan <atbrenna@lakeheadu.ca>
2024-02-10 18:09:20 -0500

    Added basic db models

---
+++ Barbershop/Models/Appointment.cs
@@ -0,0 +1,13 @@
+﻿namespace Barbershop.Models
+{
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+        public DateTime AppointmentDateTime { get; set; }
+
+        public int BarberId { get; set; }
+
+        // child ref
+        public List<Barber> Barber { get; set; }
+    }
+}
---
+++ Barbershop/Models/Barber.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Barbershop.Models
+{
+    public class Barber
+    {
+
+        public int BarberId { get; set; }
+        public string BarberName { get; set; }
+
+        // parent ref
+        public Appointment Appointment { get; set; }
+    }
+}
